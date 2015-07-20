736f0071f6c72dbdc7ee02fe6833f57bdd16c103
j-keravica <jelena.keravica@gmail.com>
2015-07-20 12:46:17 +0200

    Initial commit
    Sinatra app with two blocks for receiving color information and strings which then packes them together. Two color options are available: black and random (black, red, green).

---
+++ main.rb
@@ -0,0 +1,36 @@
+require 'sinatra/base'
+require 'rest-client'
+
+class SinatraApp < Sinatra::Base
+
+	configure do
+		enable :sessions
+	end
+
+	helpers do
+	  def choose_color(color)
+	    if color == 'b'
+	    	return '#000000'
+	    else
+	    	return ['#000000', '#FF0000', '#00A400'].sample
+	    end
+	  end
+	end
+
+	#send the (word, color) pair to be pushed back to the client
+	get '/color/:word' do
+	  color = request.cookies['color']
+	  word = params[:word]
+	  puts "Your #{word} will be of color #{color}"
+	  puts RestClient.get 'http://localhost:5678/send', {:params => {:word => word, :color => choose_color(color)}}
+	end
+
+	#store color information for the user
+	get '/set/:color' do
+		color = params[:color]
+		response.set_cookie "color", color
+	end
+
+	run! if app_file == $0
+
+end

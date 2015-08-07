c53fe6cfecf251f9c683c0488c111d73e8d67a71
j-keravica <jelena.keravica@gmail.com>
2015-08-07 09:33:06 +0200

    Added 'dotenv' to Gemfile

--- environment.rb
+++ environment.rb
@@ -1,11 +1,11 @@
-require 'rubygems'
-require 'bundler'
-
+require "rubygems"
+require "bundler"
+require "dotenv"
 
 Bundler.require(:default)                   # load all the default gems
 Bundler.require(Sinatra::Base.environment)  # load all the environment specific gems
+Dotenv.load
 
 ENV["RACK_ENV"] = "test"
-ENV["SEND_URL"] = 'http://localhost:5678'
 
 require './text_color.rb'
